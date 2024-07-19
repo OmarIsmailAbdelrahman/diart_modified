5025e067b5639adadd43b3bc92d2f20bc449d780
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-19 10:09:30 +0300

    Update diarization.py

--- src/diart/blocks/diarization.py
+++ src/diart/blocks/diarization.py
@@ -190,7 +190,7 @@ class SpeakerDiarization(base.Pipeline):
         embeddings = self.embedding(batch, segmentations)
         seg_resolution = waveforms[0].extent.duration / segmentations.shape[1]
         
-        print(f"legendary-SpeakerDiarization-__call__ batch {batch.shape} segmentation {segmentations.shape} {segmentations} embedding {embeddings.shape} {embeddings}")
+        print(f"legendary-SpeakerDiarization-__call__ batch {batch.shape} segmentation {segmentations.shape} embedding {embeddings.shape}")
 
         outputs = []
         for wav, seg, emb in zip(waveforms, segmentations, embeddings):
