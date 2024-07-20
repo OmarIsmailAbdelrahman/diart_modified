3dff8c597c6e97984630923d2e33572818e1ca4c
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-20 13:15:17 +0300

    Update diarization.py

--- src/diart/blocks/diarization.py
+++ src/diart/blocks/diarization.py
@@ -206,7 +206,7 @@ class SpeakerDiarization(base.Pipeline):
 
         
         processed_signal = batch.reshape(batch_size, 1, -1).to(torch.float)
-        processed_signal_length = batch[1].reshape(-1)
+        processed_signal_length = batch[:,:,0].reshape(-1)
         print(f"legendary-SpeakerDiarization-__call__ processed_signal  {processed_signal.shape} processed_signal_length {processed_signal_length} {processed_signal_length.shape}")
         output = vad_model(processed_signal=processed_signal, processed_signal_length=processed_signal_length)
         #output = vad_model(processed_signal=batch.reshape(batch_size,1,-1).to(torch.float), processed_signal_length=batch.reshape(-1).shape/batch_size)############################################################################################################################################
