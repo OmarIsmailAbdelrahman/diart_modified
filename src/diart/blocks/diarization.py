e7a750c1c2d92bb0f39134ef499114d25edc4288
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-21 10:40:59 +0300

    Update diarization.py

--- src/diart/blocks/diarization.py
+++ src/diart/blocks/diarization.py
@@ -248,9 +248,11 @@ class SpeakerDiarization(base.Pipeline):
         assert batch.shape[1] == expected_num_samples, msg
 
         ############################################################
-        signal = batch.reshape(-1).to(torch.float).to(device)
+        signal = batch.to(torch.float).to(device)
+        print(f"Legendary signal shape {signal.shape}")
         temp = prepare_input_from_array(signal)
-        input_signal , input_signal_length = torch.tensor(temp).reshape(1,temp.shape[0],-1), torch.tensor([temp.shape[1] for i in range(temp.shape[0])]).long()
+        print(f"Legendary temp shape {temp.shape}")
+        input_signal , input_signal_length = torch.tensor(temp).reshape(batch_size,temp.shape[0],-1), torch.tensor([temp.shape[1] for i in range(temp.shape[0])]).long()
         #input_signal_length = [x.shape[0] for x in input_signal]
         print(f"legendary-SpeakerDiarization-__call__ processed_signal  {input_signal.shape} processed_signal_length {input_signal_length} shape {input_signal_length.shape}")
         vad_output = vad_model(processed_signal=input_signal,processed_signal_length=input_signal_length)
