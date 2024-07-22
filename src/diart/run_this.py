cbbd2c4e5cc64653b9683df26aa3e2e827438230
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-22 12:46:59 +0300

    Update run_this.py

--- src/diart/run_this.py
+++ src/diart/run_this.py
@@ -240,8 +240,8 @@ logging.getLogger("whisper_timestamped").setLevel(logging.ERROR)
 #segmentation = SegmentationModel(segmentation_loader)
 
 config = SpeakerDiarizationConfig (
-    duration=2,
-    step=2,
+    duration=4,
+    step=0.5,
     latency="min",
     tau_active=0.5,
     rho_update=0.1,
