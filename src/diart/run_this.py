a0562ceb088eef38ec6bf5b4b7509273acd55387
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-22 13:35:14 +0300

    Update run_this.py

--- src/diart/run_this.py
+++ src/diart/run_this.py
@@ -241,7 +241,7 @@ logging.getLogger("whisper_timestamped").setLevel(logging.ERROR)
 
 config = SpeakerDiarizationConfig (
     duration=4,
-    step=0.5,
+    step=2,
     latency="min",
     tau_active=0.5,
     rho_update=0.1,
