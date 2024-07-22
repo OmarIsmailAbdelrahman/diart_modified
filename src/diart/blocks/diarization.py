d9bd9e465467dc878c31e61764e9034ade53660e
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-22 08:54:48 +0300

    Update diarization.py

--- src/diart/blocks/diarization.py
+++ src/diart/blocks/diarization.py
@@ -349,8 +349,8 @@ class SpeakerDiarization(base.Pipeline):
         segments = segment_audio(batch.reshape(-1), start_timestamps, end_timestamps, sample_rate=16000)
         print(f"Legendary number of segments created from batch {len(segments)} segment sizes {[len(segment[0]) for segment in segments] }")
         subsegments,subseg_start, subseg_ends = create_subsegments_from_segments(segments, self.global_offset, sample_rate=16000, window=0.63, shift=0.08)
-        self.global_offset += batch.reshape(-1) / 16000
-        print(f"Legendary number of subSegments created from batch {len(subsegments)} segment sizes {[len(segment) for segment in subsegments] }")
+        self.global_offset += len(batch.reshape(-1)) / 16000
+        print(f"Legendary number of subSegments created from batch {len(subsegments)} segment sizes {[len(segment) for segment in subsegments] } global offset {self.global_offset}")
         
         emd_tita_net = get_embeddings(subsegments)
         print(f"Legendary emd_tita_net {emd_tita_net.shape}")
