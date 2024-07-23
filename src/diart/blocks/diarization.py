9baf1c566e14024111665da67bc74525d6ef9028
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-23 13:26:37 +0300

    Update diarization.py

--- src/diart/blocks/diarization.py
+++ src/diart/blocks/diarization.py
@@ -519,12 +519,12 @@ class SpeakerDiarization(base.Pipeline):
         print(f"global number {len(self.embedding_arr)}")
         
         self.global_offset += 0.5 # step size
-        tempoooo = [[x[3],x[4]] for x in self.embedding_arr]
+        tempoooo = np.array([[x[3],x[4]] for x in self.embedding_arr])
         print(tempoooo)
         print(f" torch.tensor(self.embedding_arr) {torch.tensor(len(self.embedding_arr))} emd_tita_net {emd_tita_net.shape}")
         tempo = speaker_clustering.forward_infer(
             embeddings_in_scales=torch.stack([x[1] for x in self.embedding_arr]).to(torch.float),
-            timestamps_in_scales=torch.stack(tempoooo),
+            timestamps_in_scales=torch.tensor(tempoooo),
             multiscale_segment_counts= torch.tensor([len(self.embedding_arr)]),
             multiscale_weights=torch.tensor([1]),
             oracle_num_speakers=-1,
