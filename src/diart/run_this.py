c24fb79d616489e6740b4dc61e012f0708b158ed
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-23 16:27:11 +0300

    Update run_this.py

--- src/diart/run_this.py
+++ src/diart/run_this.py
@@ -38,71 +38,6 @@ def suppress_stdout():
         finally:
             sys.stdout = old_stdout
 
-# class WhisperTranscriber:
-#     def __init__(self, model="small", device=None):
-#         self.model = whisper.load_model(model, device=device)
-#         self._buffer = ""
-
-#     def transcribe(self, waveform):
-#         """Transcribe audio using Whisper"""
-#         # Pad/trim audio to fit 30 seconds as required by Whisper
-#         audio = waveform.data.astype("float32").reshape(-1)
-#         audio = whisper.pad_or_trim(audio)
-
-#         # Transcribe the given audio while suppressing logs
-#         with suppress_stdout():
-#             transcription = whisper.transcribe(
-#                 self.model,
-#                 audio,
-#                 # We use past transcriptions to condition the model
-#                 initial_prompt=self._buffer,
-#                 verbose=True  # to avoid progress bar
-#             )
-
-#         return transcription
-
-#     def identify_speakers(self, transcription, diarization, time_shift):
-#         """Iterate over transcription segments to assign speakers"""
-#         speaker_captions = []
-#         for segment in transcription["segments"]:
-
-#             # Crop diarization to the segment timestamps
-#             start = time_shift + segment["words"][0]["start"]
-#             end = time_shift + segment["words"][-1]["end"]
-#             dia = diarization.crop(Segment(start, end))
-
-#             # Assign a speaker to the segment based on diarization
-#             speakers = dia.labels()
-#             num_speakers = len(speakers)
-#             if num_speakers == 0:
-#                 # No speakers were detected
-#                 caption = (-1, segment["text"])
-#             elif num_speakers == 1:
-#                 # Only one speaker is active in this segment
-#                 spk_id = int(speakers[0].split("speaker")[1])
-#                 caption = (spk_id, segment["text"])
-#             else:
-#                 # Multiple speakers, select the one that speaks the most
-#                 max_speaker = int(np.argmax([
-#                     dia.label_duration(spk) for spk in speakers
-#                 ]))
-#                 caption = (max_speaker, segment["text"])
-#             speaker_captions.append(caption)
-
-#         return speaker_captions
-
-#     def __call__(self, diarization, waveform):
-#         # Step 1: Transcribe
-#         transcription = self.transcribe(waveform)
-#         # Update transcription buffer
-#         self._buffer += transcription["text"]
-#         # The audio may not be the beginning of the conversation
-#         time_shift = waveform.sliding_window.start
-#         # Step 2: Assign speakers
-#         speaker_transcriptions = self.identify_speakers(transcription, diarization, time_shift)
-#         return speaker_transcriptions
-
-
 
 class Wav2Vec2Transcriber:
     def __init__(self, model_name="othrif/wav2vec2-large-xlsr-egyptian"):
@@ -173,28 +108,9 @@ class Wav2Vec2Transcriber:
 import numpy as np
 from pyannote.core import Annotation, SlidingWindowFeature, SlidingWindow
 
-def concat(chunks, collar=0.05):
-    """
-    Concatenate predictions and audio
-    given a list of `(diarization, waveform)` pairs
-    and merge contiguous single-speaker regions
-    with pauses shorter than `collar` seconds.
-    """
-    first_annotation = chunks[0][0]
-    first_waveform = chunks[0][1]
-    annotation = Annotation(uri=first_annotation.uri)
-    data = []
-    for ann, wav in chunks:
-        annotation.update(ann)
-        data.append(wav.data)
-    annotation = annotation.support(collar)
-    window = SlidingWindow(
-        first_waveform.sliding_window.duration,
-        first_waveform.sliding_window.step,
-        first_waveform.sliding_window.start,
-    )
-    data = np.concatenate(data, axis=0)
-    return annotation, SlidingWindowFeature(data, window)
+def splitter(x):
+    print(x)
+    return x
 
 def colorize_transcription(transcription):
     """
@@ -267,7 +183,7 @@ source.stream.pipe(
     ops.buffer_with_count(count=1),
     ops.map(print2),
     ops.map(dia),
-    ops.map(concat),
+    ops.map(splitter),
     ops.filter(lambda ann_wav: ann_wav[0].get_timeline().duration() > 0),
     ops.starmap(asr),
     ops.map(colorize_transcription),
