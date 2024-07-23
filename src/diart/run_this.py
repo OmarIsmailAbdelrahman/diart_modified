70afafdd6a014308ef10f8ba038405204b16c12d
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-23 20:00:08 +0300

    Update run_this.py

--- src/diart/run_this.py
+++ src/diart/run_this.py
@@ -174,7 +174,7 @@ batch_size = int(transcription_duration // config.step)
 sample_rate = 16000
 window = 5
 step_size = 0.5
-min_update_size = sample_rate * step_size
+chunk_size = sample_rate * step_size
 required_length = sample_rate * window
 
 def update_accumulated_data(acc, new_data):
