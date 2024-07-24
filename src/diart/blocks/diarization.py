c371c89938903fa5b6ce24398c4587c2c5cd1069
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-24 11:06:19 +0300

    Update diarization.py

--- src/diart/blocks/diarization.py
+++ src/diart/blocks/diarization.py
@@ -307,9 +307,7 @@ class EmbeddingGraph:
 
         for embedding_id, label in zip(embedding_arr, clustering_prediction):
             self.add_embedding(embedding_id)
-            print(f"int(label) {int(label)}, embedding_id {embedding_id}")
             label_to_embeddings[int(label)].append(embedding_id)
-            print(f"label_to_embeddings[label] {label_to_embeddings[label]}, label {label}")
 
         # Update edges within each label group
         for embedding_ids in label_to_embeddings.values():
